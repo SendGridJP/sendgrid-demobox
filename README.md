56aef6608327d4c920cc48ea26e30fea1475e0f2
awwa500@gmail.com <awwa500@gmail.com>
2014-09-16 15:23:43 +0900

    Add encode

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ sendgrid-demo
 =============
 
 TODO
+ISO-2022-JPエンコード表示対応（receive.jsにEncoding処理を噛ませてみたがISO-2022-JPが正しくエンコードされない）
 Heroku対応
 Spec追加
 README記載
--- src/public/js/receive.js
+++ src/public/js/receive.js
@@ -3,12 +3,29 @@ $(function(){
   $("#nav-send").removeClass("active");
   $("#nav-receive").addClass("active");
 
+  str2array = function(str) {
+      var array = [],i,il=str.length;
+      for(i=0;i<il;i++) array.push(str.charCodeAt(i));
+      return array;
+  };
+
+  decode = function(text, encode){
+    if (encode == "ISO-2022-JP") {
+      alert("iso");
+      var arr = Encoding.convert(text, 'JIS');
+      return Encoding.codeToString(arr);
+    }
+    return text;
+  };
+
   getRow = function(receive){
-    var to = ("to" in receive)? receive.to : "";
-    var from = ("from" in receive)? receive.from : "";
-    var subject = ("subject" in receive)? receive.subject : "";
-    var text = ("text" in receive)? receive.text : "";
     var charsets = ("charsets" in receive)? receive.charsets : "";
+    var cs = JSON.parse(receive.charsets);
+
+    var to = ("to" in receive)? decode(receive.to, cs.to) : "";
+    var from = ("from" in receive)? decode(receive.from, cs.from) : "";
+    var subject = ("subject" in receive)? decode(receive.subject, cs.subject) : "";
+    var text = ("text" in receive)? decode(str2array(receive.text), cs.text) : "";
     var attachments = ("attachments" in receive)? receive.attachments : "";
     var envelope = ("envelope" in receive)? receive.envelope : "";
     var sender_ip = ("sender_ip" in receive)? receive.sender_ip : "";
