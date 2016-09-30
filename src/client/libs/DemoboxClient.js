eebe04355d2d25da4e271cf8fecf1702b20373a1
awwa500@gmail.com <awwa500@gmail.com>
2016-10-01 05:23:30 +0900

    implement removeEmpty

--- src/client/libs/DemoboxClient.js
+++ src/client/libs/DemoboxClient.js
@@ -16,34 +16,10 @@ var DemoboxClient = {
       mailData2.personalizations[i].headers = this.array2hash(mailData.personalizations[i].headers);
       mailData2.personalizations[i].substitutions = this.array2hash(mailData.personalizations[i].substitutions);
       mailData2.personalizations[i].custom_args = this.array2hash(mailData.personalizations[i].custom_args);
-      if (mailData2.personalizations[i].cc.length === 0) {
-        delete mailData2.personalizations[i].cc;
-      }
-      if (mailData2.personalizations[i].bcc.length === 0) {
-        delete mailData2.personalizations[i].bcc;
-      }
-      if (mailData2.personalizations[i].subject === null) {
-        delete mailData2.personalizations[i].subject;
-      }
-      if (mailData2.personalizations[i].headers === null) {
-        delete mailData2.personalizations[i].headers;
-      }
-      if (mailData2.personalizations[i].substitutions === null) {
-        delete mailData2.personalizations[i].substitutions;
-      }
-      if (mailData2.personalizations[i].custom_args === null) {
-        delete mailData2.personalizations[i].custom_args;
-      }
-      if (mailData2.personalizations[i].send_at === null) {
-        delete mailData2.personalizations[i].send_at;
-      }
     }
-    if (mailData2.subject === null) {
-      delete mailData2.subject;
-    }
-    if (mailData2["reply-to"] === null) {
-      delete mailData2["reply-to"];
-    }
+
+    console.log(JSON.stringify(mailData));
+    this.removeEmpty(mailData2);
 
     var requestParam = JSON.stringify(mailData2);
     console.log(requestParam);
@@ -51,11 +27,33 @@ var DemoboxClient = {
   },
 
   array2hash: function(array) {
-    var hash = null;
+    var hash = {};
     for (var j = 0; j < array.length; j++) {
       hash[array[j].key] = array[j].value;
     }
     return hash;
+  },
+
+  removeEmpty: function(element) {
+    // Array
+    if (_.isArray(element)) {
+      // Loop with element and recursive call removeEmpty()
+      _.each(element, function(e) {
+        this.removeEmpty(e);
+      }.bind(this));
+    }
+    // Object
+    if (_.isObject(element)) {
+      // Loop with object property
+      _.each(_.keys(element), function(key) {
+        // delete property if the property value is null,[],{}
+        if ((element[key] === null) || (_.isObject(element[key]) && _.isEmpty(element[key])) ){
+          delete element[key];
+        } else {
+          this.removeEmpty(element[key]);
+        }
+      }.bind(this));
+    }
   }
 };
 
